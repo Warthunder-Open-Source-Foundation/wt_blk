71d7178cebdb602acf7714099ec4d2c0ed921eaa
FlareFlo <mail@flareflo.dev>
2023-02-22 00:23:11 +0100

    Add support for slim ZSTD file

--- src/binary/zstd.rs
+++ src/binary/zstd.rs
@@ -3,20 +3,28 @@ use ruzstd::StreamingDecoder;
 
 pub fn decode_zstd(file: &[u8]) -> Option<Vec<u8>> {
 	// validate magic byte
-	if file[0] != 0x2 {
-		return None;
-	}
+	let is_fat = match file.get(0)? {
+		0x2 => true,
+		0x4 => false,
+		_ => return None
+	};
 
-	let len_raw = &file[1..4];
-	let len = u32::from_be_bytes([
-		0,
-		len_raw[2],
-		len_raw[1],
-		len_raw[0],
-	]);
-	let mut to_decode = &file[4..(len as usize + 4)];
+	let (len, mut to_decode) = if is_fat {
+		let len_raw = &file[1..4];
+		let len = u32::from_be_bytes([
+			0,
+			len_raw[2],
+			len_raw[1],
+			len_raw[0],
+		]);
+		let mut to_decode = &file[4..(len as usize + 4)];
+		(len as usize, to_decode)
+	} else {
+		(file.len() - 1, &file[1..])
+	};
+
 	let mut decoder = StreamingDecoder::new(&mut to_decode).ok()?;
-	let mut out = Vec::with_capacity(len as usize);
+	let mut out = Vec::with_capacity(len);
 	let _ = decoder.read_to_end(&mut out).ok()?;
 	Some(out)
 }
@@ -29,6 +37,12 @@ mod test {
 	#[test]
 	fn fat_zstd() {
 		let decoded = decode_zstd(include_bytes!("../../samples/section_fat_zst.blk")).unwrap();
-		assert_eq!(&decoded, include_bytes!("../../samples/section_fat.blk"))
+		assert_eq!(&decoded, &include_bytes!("../../samples/section_fat.blk"))
+	}
+
+	#[test]
+	fn slim_zstd() {
+		let decoded = decode_zstd(include_bytes!("../../samples/section_slim_zst.blk")).unwrap();
+		assert_eq!(&decoded, &include_bytes!("../../samples/section_slim.blk")[1..]) // Truncating the first byte, as it is magic byte for the SLIM format
 	}
 }
