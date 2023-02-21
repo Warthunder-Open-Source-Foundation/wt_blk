0f620d0b1612bf4e229f982c02b529558cb866c6
FlareFlo <mail@flareflo.dev>
2023-02-21 18:35:53 +0100

    Add parser and bin128

---
+++ src/binary/leb128.rs
@@ -0,0 +1,15 @@
+
+pub fn uleb128(bytes: &[u8]) -> Option<(usize, u128)> {
+	let mut result = 0u128;
+	let mask = 1 << 7;
+
+	for i in 0..bytes.len() {
+		let bits = (bytes[i] & (mask - 1)) as u128;
+		result |=  bits << (7 * i);
+
+		if mask & bytes[i] == 0 {
+			return Some((i + 1, result));
+		};
+	}
+	None
+}
