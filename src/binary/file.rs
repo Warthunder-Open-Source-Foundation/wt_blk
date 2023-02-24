849a6ac41000a92207fcd3511e09dcadd18d3477
flareflo <github@flareflo.dev>
2023-02-24 21:36:39 +0100

    Remove deprecated struct

--- src/binary/file.rs
+++ src/binary/file.rs
@@ -6,13 +6,3 @@ pub enum FileType {
 	SLIM_ZSTD = 0x04,
 	SLIM_ZST_DICT = 0x05,
 }
-
-#[derive(Debug)]
-pub struct FatBLk {
-	pub names_count: usize,
-	pub names_data_size: usize,
-	pub names: Vec<String>,
-	pub blocks_count: usize,
-	pub params_count: usize,
-	pub params_data_size: usize,
-}
