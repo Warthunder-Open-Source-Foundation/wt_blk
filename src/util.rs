0837f75c509ce67dded7d91585af62c2c753e849
FlareFlo <mail@flareflo.dev>
2023-03-06 00:44:06 +0100

    remove brackets from time statement

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ macro_rules! time {
     ($e:expr) => {{
         let start = std::time::Instant::now();
         let output = $e;
-        println!("[{}:{} {:?}]", file!{}, line!{},start.elapsed());
+        println!("{}:{} {:?}", file!{}, line!{},start.elapsed());
         drop(start);
         output
     }};
