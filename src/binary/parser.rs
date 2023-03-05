6a19c441f200abc7f6d167e2e1812243165b2685
FlareFlo <mail@flareflo.dev>
2023-03-06 00:39:50 +0100

    Remove unused import

--- src/binary/parser.rs
+++ src/binary/parser.rs
@@ -4,7 +4,7 @@ use crate::binary::blk_structure::BlkField;
 use crate::binary::blk_type::BlkType;
 use crate::binary::file::FileType;
 use crate::binary::leb128::uleb128;
-use crate::binary::nm_file::{parse_name_section, parse_slim_nm};
+use crate::binary::nm_file::{parse_name_section};
 
 //															TODO: Pass the name map parsed as a finished `&[String]`
 pub fn parse_blk(file: &[u8], with_magic_byte: bool, is_slim: bool, name_map: Option<&[u8]>, parsed_nm: Rc<Vec<String>>) -> BlkField {
