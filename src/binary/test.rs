1ced4368038b0c3749e10964a0a3c493b95efd19
FlareFlo <mail@flareflo.dev>
2023-03-05 22:18:33 +0100

    Add timing macro for benchmarking calls

--- src/binary/test.rs
+++ src/binary/test.rs
@@ -21,6 +21,7 @@ mod test {
 	use std::rc::Rc;
 	use std::sync::atomic::{AtomicUsize, Ordering};
 	use std::time::Instant;
+	use wt_blk_macro::time;
 
 	use crate::binary::blk_type::BlkType;
 	use crate::binary::leb128::uleb128;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use wt_blk_macro::time;
+
 pub mod binary;
 pub mod output_parsing;
 mod io;
