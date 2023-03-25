c6e5397001176d86e09223c1c8705b95c0901bdf
flareflo <github@flareflo.dev>
2023-03-25 21:59:38 +0100

    Permit dead code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+#![allow(unused)] // Building the API means those warnings are just noisy TODO: Remove
+
+
 #![feature(arc_unwrap_or_clone)]
 
 pub mod binary;
 pub mod output_parsing;
 mod io;
 mod util;
+mod wrpl;
