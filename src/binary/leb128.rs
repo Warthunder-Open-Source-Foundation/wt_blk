c60c81cba96cd30c5a2a92942008a5eebfafd2be
flareflo <github@flareflo.dev>
2023-03-27 03:40:15 +0200

    Enumerate instead of indexing into iterable

--- src/binary/leb128.rs
+++ src/binary/leb128.rs
@@ -6,11 +6,11 @@ pub fn uleb128(bytes: &[u8]) -> Result<(usize, usize), ParseError> {
 	let mut result = 0u128;
 	let mask = 1 << 7;
 
-	for i in 0..bytes.len() {
-		let bits = (bytes[i] & (mask - 1)) as u128;
+	for (i, current) in bytes.iter().enumerate() {
+		let bits = (current & (mask - 1)) as u128;
 		result |=  bits << (7 * i);
 
-		if mask & bytes[i] == 0 {
+		if mask & current == 0 {
 			return Ok((i + 1, result as usize));
 		};
 	}
