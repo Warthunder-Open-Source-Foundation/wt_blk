169ccd3824686f43ef40f0334d0530a5d221def0
flareflo <github@flareflo.dev>
2023-04-09 02:42:56 +0200

    Create and document inner container

--- src/vromf/mod.rs
+++ src/vromf/mod.rs
@@ -1,4 +1,11 @@
 mod enums;
 mod util;
 mod de_obfuscation;
+
+/// It is generally expected to directly call into the public interfaces from this module, ignoring the inner lower-level functions
+
+// This module unpacks the "outer" shell of the vromf image
 mod binary_container;
+
+// This module unpacks the inner parts of the binary image
+mod inner_container;
