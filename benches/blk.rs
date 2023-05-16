3cfd05f124b91c224f72f0aa85294b256b1ad037
FlareFlo <mail@flareflo.dev>
2023-05-16 15:26:41 +0200

    Add blk test

--- benches/blk.rs
+++ benches/blk.rs
@@ -1,48 +1,23 @@
-use std::{fs, rc::Rc};
-
+use std::path::PathBuf;
+use std::str::FromStr;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ruzstd::FrameDecoder;
-use wt_blk::blk::{
-	leb128::uleb128,
-	nm_file::{decode_nm_file, parse_name_section},
-	parser::parse_blk,
-	zstd::{decode_zstd, eep},
-};
+use wt_blk::blk::BlkOutputFormat;
+use wt_blk::blk::output_formatting_conf::FormattingConfiguration;
+use wt_blk::vromf::unpacker::VromfUnpacker;
 
-pub fn blk_fat(c: &mut Criterion) {
-	let f = fs::read("./samples/section_fat.blk").unwrap();
-	c.bench_function("blk fat", |b| {
-		b.iter(|| parse_blk(black_box(&f), true, false, None, Rc::new(vec![])))
+
+fn blk_ref_json(c: &mut Criterion) {
+	let vromf = include_bytes!("../samples/aces.vromfs.bin");
+	let serialized = VromfUnpacker::from_file((PathBuf::from_str("../samples/aces.vromfs.bin").unwrap(), vromf.to_vec())).unwrap();
+	c.bench_function("blk as_ref_json all", |b|{
+		b.iter(
+			|| {
+
+			}
+		);
 	});
 }
 
-pub fn zstd(c: &mut Criterion) {
-	let f = fs::read("./samples/section_fat_zst.blk").unwrap();
-	c.bench_function("zstd", |b| {
-		b.iter(|| decode_zstd(black_box(&f), Rc::new(FrameDecoder::new())))
-	});
-}
 
-pub fn nm_section(c: &mut Criterion) {
-	let nm = fs::read("../wt_blk/samples/rendist/nm").unwrap();
-	let nm = decode_nm_file(&nm).unwrap();
-
-	let mut nm_ptr = 0;
-
-	let (offset, names_count) = uleb128(&nm[nm_ptr..]).unwrap();
-	nm_ptr += offset;
-
-	let (offset, names_data_size) = uleb128(&nm[nm_ptr..]).unwrap();
-	nm_ptr += offset;
-
-	c.bench_function("nm_section", |b| {
-		b.iter(|| parse_name_section(black_box(&nm[nm_ptr..(nm_ptr + names_data_size)])))
-	});
-}
-
-pub fn eep_bench(c: &mut Criterion) {
-	c.bench_function("eep", |b| b.iter(|| eep()));
-}
-
-criterion_group!(benches, nm_section);
+criterion_group!(benches, blk_ref_json);
 criterion_main!(benches);
