87242156ae467d9aee89ac296875480585322504
FlareFlo <mail@flareflo.dev>
2023-08-26 01:01:14 +0200

    Remove another dead module, and document blk module structure

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,17 @@
 
 extern crate core;
 
+/// Low-level functions for BLK file format, for high level API use the [`vromf`] module
 pub mod blk;
+
+/// Misc. utility functions for the DXP and GRP file-format
 pub mod dxp_and_grp;
-pub mod output_parsing;
+
+/// General utility functions used in the entire crate
 mod util;
+
+/// High-level API for unpacking entire Vromf archives
 pub mod vromf;
+
+/// Experimental WRPL unpacking (WIP)
 mod wrpl;
