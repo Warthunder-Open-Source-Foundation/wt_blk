c64e0fa3ae32f26d40d2cde76a08db3ddbd113f6
FlareFlo <mail@flareflo.dev>
2023-10-18 01:51:17 +0200

    Add benchmark harness

---
+++ benches/blk.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use wt_blk::blk::parser::parse_blk;
+use divan::black_box;
+
+fn main() {
+	// Run registered benchmarks.
+	divan::main();
+}
+
+const FILE: &[u8] = include_bytes!("../samples/section_fat.blk");
+
+// Define a `fibonacci` function and register it for benchmarking.
+#[divan::bench]
+fn parse() {
+	let file = black_box(FILE);
+	let mut output = parse_blk(black_box(&file[1..]), black_box(false), black_box(None)).unwrap();
+	for _ in 0..10000 {
+		output = parse_blk(black_box(&file[1..]), black_box(false), black_box(None)).unwrap();
+	}
+	if output.estimate_size() == 0 && file.len() == 0 {
+		panic!("infallible benchmark harness")
+	}
+}
