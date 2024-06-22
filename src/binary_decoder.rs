34f52234e52f71565f2e440cdede456567250740
FlareFlo <mail@flareflo.dev>
2024-06-22 02:07:34 +0200

    Refactor struct

--- src/binary_decoder.rs
+++ src/binary_decoder.rs
@@ -6,20 +6,20 @@ use crate::blk::{
 	leb128::uleb128,
 };
 
-type BlkResult<T> = Result<T, ParseError>;
+type BinaryDecoderResult<T> = Result<T, ParseError>;
 
-pub struct BlkDecoder<'a> {
+pub struct BinaryDecoder<'a> {
 	bytes:  &'a [u8],
 	cursor: usize,
 }
 
-impl<'a> BlkDecoder<'a> {
+impl<'a> BinaryDecoder<'a> {
 	pub fn new(bytes: &'a [u8]) -> Self {
 		Self { bytes, cursor: 0 }
 	}
 
 	/// Returns the next bytes at the current cursor
-	pub fn next_bytes(&self) -> BlkResult<&'a [u8]> {
+	pub fn next_bytes(&self) -> BinaryDecoderResult<&'a [u8]> {
 		if self.cursor < self.bytes.len() {
 			Ok(self.bytes.index(self.cursor..))
 		} else {
@@ -36,7 +36,7 @@ impl<'a> BlkDecoder<'a> {
 	}
 
 	/// Checked backwards-seeking operation
-	pub fn seek_back(&mut self, by: usize) -> BlkResult<()> {
+	pub fn seek_back(&mut self, by: usize) -> BinaryDecoderResult<()> {
 		self.cursor = self
 			.cursor
 			.checked_add(by)
@@ -49,7 +49,7 @@ impl<'a> BlkDecoder<'a> {
 	}
 
 	/// Returns next uleb encoded integer, advancing the cursor
-	pub fn next_uleb(&mut self) -> BlkResult<usize> {
+	pub fn next_uleb(&mut self) -> BinaryDecoderResult<usize> {
 		let bytes = self.next_bytes()?;
 		let (uleb_len, value) = uleb128(bytes)?;
 		self.seek(uleb_len);
