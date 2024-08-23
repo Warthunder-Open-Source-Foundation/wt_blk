41f07d44c5f93a872a2749420ecbbe5a9042dbee
axiangcoding <axiangcoding@gmail.com>
2024-08-23 19:09:34 +0800

    update

--- python_bindings/wt_blk_pybindings.pyi
+++ python_bindings/wt_blk_pybindings.pyi
@@ -1,4 +1,18 @@
-from typing import Optional, List
+from typing import Optional
+
 class wt_blk_pybindings:
     """Python bindings for wt_blk"""
-    def binary_blk_to_json(blk: bytes, dict: Optional[bytes] = None, nm: Optional[bytes] = None) -> str:
+    def binary_blk_to_json(
+        blk: bytes, dict: Optional[bytes] = None, nm: Optional[bytes] = None
+    ) -> str: 
+        """Converts a blk format binary block to a JSON string
+
+        Args:
+            blk (bytes): blk format binary block
+            dict (Optional[bytes], optional): Defaults to None.
+            nm (Optional[bytes], optional): Defaults to None.
+
+        Returns:
+            str: JSON string
+        """
+        ...
