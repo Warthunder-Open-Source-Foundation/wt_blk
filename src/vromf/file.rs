e7789771516198612f416a0606c7a7504adcce5f
FlareFlo <mail@flareflo.dev>
2024-08-31 16:10:32 +0200

    Add metadata to file struct

--- src/vromf/file.rs
+++ src/vromf/file.rs
@@ -2,24 +2,36 @@ use std::{
 	fs,
 	path::{Path, PathBuf},
 };
+use std::fs::Metadata;
+use std::io::Read;
 
 #[derive(Debug, Clone)]
 pub struct File {
 	path: PathBuf,
 	file: Vec<u8>,
+	// Present when read from disk, not present when created from VROMF
+	meta: Option<Metadata>,
 }
 
 impl File {
 	pub fn new(p: impl Into<PathBuf>) -> color_eyre::Result<Self> {
 		let path = p.into();
+		let mut f  = fs::File::open(&path)?;
+		let mut buf = Vec::with_capacity(1024);
+		f.read_to_end(&mut buf)?;
 		Ok(Self {
-			file: fs::read(&path)?,
+			file: buf,
 			path,
+			meta: Some(f.metadata()?),
 		})
 	}
 
+	pub fn from_raw_with_meta(path: PathBuf, file: Vec<u8>, meta: Metadata) -> Self {
+		Self { path, file, meta: Some(meta) }
+	}
+
 	pub fn from_raw(path: PathBuf, file: Vec<u8>) -> Self {
-		Self { path, file }
+		Self { path, file, meta: None }
 	}
 
 	pub fn split(self) -> (PathBuf, Vec<u8>) {
@@ -44,5 +56,9 @@ impl File {
 
 	pub fn as_ref(&self) -> (&Path, &[u8]) {
 		(self.path(), self.buf())
+	}
+
+	pub fn meta(&self) -> &Option<Metadata> {
+		&self.meta
 	}
 }
