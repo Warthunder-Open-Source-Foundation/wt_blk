d45337bd4cafc611a608bdab1186169dfed027a8
FlareFlo <mail@flareflo.dev>
2025-01-06 19:49:18 +0100

    Start adding bytes crate for unpacking

--- src/vromf/binary_container.rs
+++ src/vromf/binary_container.rs
@@ -1,5 +1,5 @@
 use std::mem::size_of;
-
+use bytes::{Buf, Bytes};
 use color_eyre::{eyre::bail, Report, Section};
 use wt_version::Version;
 
@@ -11,59 +11,55 @@ use crate::vromf::{
 };
 
 pub(crate) fn decode_bin_vromf(file: &[u8], validate: bool) -> Result<(Vec<u8>, Metadata), Report> {
+	let mut file = Bytes::from_owner(file);
 	let mut metadata = Metadata::default();
 
-	let mut ptr = 0_usize;
+	// let mut ptr = 0_usize;
+	//
+	// // Returns slice offset from file, incrementing the ptr by offset
+	// let idx_file_offset = |ptr: &mut usize, offset: usize| {
+	// 	if let Some(buff) = file.get(*ptr..(*ptr + offset)) {
+	// 		*ptr += offset;
+	// 		Ok(buff)
+	// 	} else {
+	// 		Err(Report::msg(format!(
+	// 			"Indexing buffer of size {} with index {} and length {}",
+	// 			file.len(),
+	// 			*ptr,
+	// 			offset
+	// 		)))
+	// 	}
+	// };
 
-	// Returns slice offset from file, incrementing the ptr by offset
-	let idx_file_offset = |ptr: &mut usize, offset: usize| {
-		if let Some(buff) = file.get(*ptr..(*ptr + offset)) {
-			*ptr += offset;
-			Ok(buff)
-		} else {
-			Err(Report::msg(format!(
-				"Indexing buffer of size {} with index {} and length {}",
-				file.len(),
-				*ptr,
-				offset
-			)))
-		}
-	};
+	metadata.header_type = Some(HeaderType::try_from(file.get_u32_le())?);
 
-	let header_type = bytes_to_int(idx_file_offset(&mut ptr, 4)?)?;
-	let header_type = HeaderType::try_from(header_type)?;
-	metadata.header_type = Some(header_type);
-
-	let platform_raw = bytes_to_int(idx_file_offset(&mut ptr, 4)?)?;
-	let platform = PlatformType::try_from(platform_raw)?;
-	metadata.platform = Some(platform);
+	metadata.platform = Some(PlatformType::try_from(file.get_u32_le())?);
 
 	// Size of the file before compression
-	let size = bytes_to_int(idx_file_offset(&mut ptr, 4)?)?;
+	let size = file.get_u32_le();
 
-	let header_packed: u32 = bytes_to_int(idx_file_offset(&mut ptr, 4)?)?;
+	let header_packed: u32 = file.get_u32_le();
 
 	// Type of compression/packing, and size before compression
 	let (pack_type, extended_header_size) = pack_type_from_aligned(header_packed)?;
 	metadata.packing = Some(pack_type);
 
-	let inner_data = if header_type.is_extended() {
-		let extended_header = idx_file_offset(
-			&mut ptr,
-			size_of::<u16>() + size_of::<u16>() + size_of::<u32>(),
-		)?;
-		let s = extended_header; // Copying ptr such that indexing below is less verbose
+	let inner_data = if HeaderType::try_from(file.get_u32_le())?.is_extended() {
+		let mut extended_header = file.split_to(size_of::<u16>() + size_of::<u16>() + size_of::<u32>());
 
 		// Unused header elements, for now
-		let _header_size = u16::from_le_bytes([s[0], s[1]]);
-		let _flags = u16::from_le_bytes([s[2], s[3]]);
-		// The version is always reversed in order. It may never exceed 255
-		let version = [s[7], s[6], s[5], s[4]];
+		let _header_size = extended_header.get_u16_le();
+		let _flags =  extended_header.get_u16_le();
+
+		let patch = extended_header.get_u8() as u16;
+		let minor = extended_header.get_u8() as u16;
+		let major = extended_header.get_u8() as u16;
+		let global = extended_header.get_u8() as u16;
 		metadata.version = Some(Version::new(
-			version[0] as u16,
-			version[1] as u16,
-			version[2] as u16,
-			version[3] as u16,
+			global,
+			major,
+			minor,
+			patch,
 		));
 
 		// Null length means the remaining bytes are used
