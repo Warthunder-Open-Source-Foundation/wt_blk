d4323e9a6f78edcee718e59b8163c6f71b713651
FlareFlo <mail@flareflo.dev>
2025-06-27 18:36:28 +0200

    Document BLK iteration 1 (WIP)

--- src/blk/mod.rs
+++ src/blk/mod.rs
@@ -1,3 +1,57 @@
+//! # Binary BLK
+//! This module describes the binary BLK format and all of its know features and flags.
+//! > ⚠ **Warning: Unless stated otherwise, all integers are Little-endian**
+//!
+//! ## Terminology
+//! For ease of writing and reading, there are a few terms i will establish for this chapter
+//! - u32 -> 32-bit unsigned integer
+//! - u64 -> 64-bit unsigned integer
+//! - f32 -> 32 bit float
+//! - f64 -> 64 bit float
+//! - \[T; N\] -> N long array of T
+//! - offset -> absolute offset from 0 in the file
+//! - nm -> A file containing Strings that is not in the BLK binary itself
+//! - dict -> A ZSTD dictionary used in batch compressing many BLK files
+//! - ULEB -> Unsigned LEB encoded variable length integer format: `https://en.wikipedia.org/wiki/LEB128`
+//!
+//! ## Types
+//! BLK has 12 data-types
+//!
+//! |Name|Byte identifier|Layout|Size in bytes|[Inline](#inlining)|
+//! |-|-|-|-|-|
+//! |String|0x01|Zero terminated string|Variable||
+//! |Int|0x02|i32|4|yes|
+//! |Float|0x03|f32|4|yes|
+//! |Float2|0x04|\[f32; 2\]|8||
+//! |Float3|0x05|\[f32; 3\]|12||
+//! |Float4|0x06|\[f32; 4\]|16||
+//! |Int2|0x07|\[i32; 2\]|8||
+//! |Int3|0x08|\[i32; 3\]|12||
+//! |Bool|0x09|boolean|4|yes|
+//! |Color|0x0a|\[u8; 4\]|4|yes|
+//! |Float12|0x0b|\[f32; 12\]|48||
+//! |Long|0x0c|i64|8||
+//!
+//! ### Inlining
+//! When a type is inline, it means that its offset field contains the types data (used for small types 32 bits or smaller).  
+//! Otherwise, the type data is an offset where the actual payload can be found.  
+//!
+//! ## Kinds of BLK files
+//! There is not just one type of BLK, there are some important differences to denote.
+//! |Byte ID|String ID|Description|
+//! |-|-|-|
+//! |0x00|BBF|A legacy format that this library does not understand|
+//! |0x01|FAT|A standalone BLK binary, about as normal as it gets|
+//! |0x02|FAT_ZST|Same as FAT, but ZSTD compressed|
+//! |0x03|SLIM|A BLK like FAT, but with all strings outlined to the nm|
+//! |0x04|SLIM_ZST|Same as SLIM, but ZSTD compressed|
+//! |0x05|SLIM_ZSTD_DICT|Same as SLIM, but ZSTD compressed using the dict|
+//! 
+//! ## File layout
+//! Now that we understand all necessary terms and types for BLK, were almost ready to encode/decode them.  
+//! I will first explain the layout of a regular FAT file, then elaborate on the differences to the other kinds.
+//! # Illustration of the fat BLK
+//! ![Illustration](https://raw.githubusercontent.com/Warthunder-Open-Source-Foundation/wt_blk/refs/heads/master/charts/rendered/fat_blk_layout.png)
 use std::{
 	fs,
 	fs::ReadDir,
