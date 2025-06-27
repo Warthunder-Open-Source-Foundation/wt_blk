2c5d7b117ef7c5325601b48a50439d3e6ca2e49c
FlareFlo <mail@flareflo.dev>
2025-06-27 19:54:35 +0200

    Fully document fat BLK

--- src/blk/mod.rs
+++ src/blk/mod.rs
@@ -13,8 +13,9 @@
 //! - nm -> A file containing Strings that is not in the BLK binary itself
 //! - dict -> A ZSTD dictionary used in batch compressing many BLK files
 //! - ULEB -> Unsigned LEB encoded variable length integer format: `https://en.wikipedia.org/wiki/LEB128`
+//! - Name/Name map -> String as key or value in the BLK, map is an array of Strings
 //!
-//! ## Types
+//! ### Types
 //! BLK has 12 data-types
 //!
 //! |Name|Byte identifier|Layout|Size in bytes|[Inline](#inlining)|
@@ -33,10 +34,10 @@
 //! |Long|0x0c|i64|8||
 //!
 //! ### Inlining
-//! When a type is inline, it means that its offset field contains the types data (used for small types 32 bits or smaller).  
-//! Otherwise, the type data is an offset where the actual payload can be found.  
+//! When a type is inline, it means that its offset field contains the types data (used for small types 32 bits or smaller).
+//! Otherwise, the type data is an offset where the actual payload can be found.
 //!
-//! ## Kinds of BLK files
+//! ### Kinds of BLK files
 //! There is not just one type of BLK, there are some important differences to denote.
 //! |Byte ID|String ID|Description|
 //! |-|-|-|
@@ -46,12 +47,93 @@
 //! |0x03|SLIM|A BLK like FAT, but with all strings outlined to the nm|
 //! |0x04|SLIM_ZST|Same as SLIM, but ZSTD compressed|
 //! |0x05|SLIM_ZSTD_DICT|Same as SLIM, but ZSTD compressed using the dict|
-//! 
+//!
+//! ### Name reference
+//! Whenever a String is used in the name-map or field-map, this layout applies.
+//! If the tag-bit is set, it means the index has to be looked up from the [external nm](#external-nm), otherwise the regular [nm](#name-map),
+//! |Tag|Index|
+//! |-|-|
+//! |1 bit| 31 bit unsigned integer|
+//!
 //! ## File layout
-//! Now that we understand all necessary terms and types for BLK, were almost ready to encode/decode them.  
+//! Now that we understand all necessary terms and types for BLK, were almost ready to encode/decode them.
 //! I will first explain the layout of a regular FAT file, then elaborate on the differences to the other kinds.
-//! # Illustration of the fat BLK
 //! ![Illustration](https://raw.githubusercontent.com/Warthunder-Open-Source-Foundation/wt_blk/refs/heads/master/charts/rendered/fat_blk_layout.png)
+//! The file starts off with a single byte describing its [kind](#kinds-of-blk-files), FAT in this case, of course.
+//! After that, we begin the first section of data already called the [Name map].
+//!
+//! ### Name Map
+//! Defines where any Strings used as keys or values in this BLK.
+//! In the case of SLIM, only the names count ULEB will be present, skipping straight to the [struct count](#struct-count).
+//! In the case of FAT, following the names count (ULEB) N is the names buffer size (ULEB) S.
+//! After this comes all null seperated strings that should be as many as N specified.
+//!
+//! ### Block count
+//! A single ULEB defining how many nested structs the BLK file contains (used later).
+//!
+//! ### Field map
+//! Similar to the name map, here we first get the amount of fields followed by the size of the payload buffer.
+//! The field payloads come before the field definitions and therefore need to be used during the decoding of the fields.
+//! Each field definition is 32 bits in size structured as such:
+//! |[Name ID](#name-reference)|Type ID|Offset|
+//! |-|-|-|
+//! |u24|u8|u32|
+//! ---
+//! When the type is inline, simply interpret the offset as its payload.
+//! Otherwise, use the offset relative to the start of the payload buffer, reading as many bytes as the type needs.
+//!
+//! ### Nesting map
+//! Until now, we have only read and parsed a list of fields. But you may ask, doesn't BLK support nested structs?
+//! Indeed, it does, and it uses the following layout to figure out which fields and struct correspond to which parent struct.
+//! The data layout is as such:
+//! |Index|Struct name|field count|sub-structs count|sub-struct index (optional)|
+//! |-|-|-|-|
+//! |ULEB|[Name ID](#name-reference)|ULEB|ULEB|
+//! ---
+//! Explaining the algorithm necessary to structure this data is not trivial, so I will explain each value and its purpose with care.
+//! However, I believe that, reading [`crate::blk::blk_block_hierarchy::FlatBlock`] (implementation) together with this text will work better than just the text.
+//!
+//! To start off, we first get the index, which uniquely identifies any struct, where 0 is the root/core struct.
+//! Together with the index, we can determine the name, which is a [Name ID](#name-reference) or undefined and irrelevant if the index is 0.
+//! 
+//! Field `count` defines how many fields from [the field map](#field-map) belong to this struct in the order as they appear.
+//! Keeping track of the sum of previous`count`'s is important as current `count` starts from where the last field ended. 
+//! 
+//! Sub-structs count defines how many substructs there are.
+//! 
+//! Sub-structs index defines which other struct are contained in this one, using the same indexing system as field count.
+//! **This value is not present when sub-structs count was 0.**
+//! 
+//! This mechanism is best explained with an example to go with it:
+//! Lets use this BLK as our working minimal example:
+//! ```blk
+//! "vec4f":p4 = 1.25, 2.5, 5, 10
+//! "int":i = 42
+//! "long":i64 = 0x40
+//! "alpha" {
+//!		"str":t = "hello"
+//!		"bool":b = true
+//!		"color":c = 0x1, 0x2, 0x3, 0x4
+//!		"gamma" {
+//!			"vec2i":ip2 = 3, 4
+//!			"vec2f":p2 = 1.25, 2.5
+//!			"transform":m = [[1, 0, 0] [0, 1, 0] [0, 0, 1] [1.25, 2.5, 5]]
+//!		}
+//! }
+//! "beta" {
+//!		"float":r = 1.25
+//!		"vec2i":ip2 = 1, 2
+//!		"vec3f":p3 = 1.25, 2.5, 5
+//! }
+//!```
+//! The Nesting map would look like the following:
+//! |Index|Name|Indexes|Sub-blocks|Binary representation|
+//! |-|-|-|-|-|
+//! |0|N.A.|0,1,2|1,2|`0x00 0x03 0x02 0x01`|
+//! |1|alpha|3,4,5|3|`0x04 0x03 0x01 0x03` |
+//! |2|beta|6,7,8||`0x0C 0x03 0x00` |
+//! |3|gamma|9,10,11||`0x08 0x03 0x00` |
+
 use std::{
 	fs,
 	fs::ReadDir,
@@ -74,8 +156,8 @@ use crate::blk::{
 	zstd::decode_zstd,
 };
 
-/// Decodes flat map of fields into the corresponding nested datastructure
-mod blk_block_hierarchy;
+/// Decodes flat map of fields into the corresponding nested datastructures
+pub(crate) mod blk_block_hierarchy;
 
 /// Defines the recursive/nested structure that BLK files are represented with internally
 pub mod blk_structure;
